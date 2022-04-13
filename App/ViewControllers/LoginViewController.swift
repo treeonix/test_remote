6161e01701e1096b5ad94aec508b56f0fd8e1039
treeonix <x.sargsyan2020@gmail.com>
2022-04-13 20:19:17 +0400

    Create LoginViewController.swift, add username, password textFields.

---
+++ App/ViewControllers/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  App
+//
+//  Created by Khachatur Hakobyan on 13.04.22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
